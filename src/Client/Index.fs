5f2dc07c83cb9186110ba6c0776a741321231832
Matt <mjg1992@hotmail.co.uk>
2020-12-14 08:27:40 +0000

    #1 Remove hero from landing page

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -67,39 +67,20 @@ let navBrand =
     ]
 
 let landingPage (model : Model) (dispatch : Msg -> unit) =
-    Hero.hero [
-        Hero.Color IsPrimary
-        Hero.IsFullHeight
-        Hero.Props [
-            Style [
-                Background """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://unsplash.it/1200/900?random") no-repeat center center fixed"""
-                BackgroundSize "cover"
-            ]
-        ]
-    ] [
-        Hero.head [ ] [
-            Navbar.navbar [ ] [
-                Container.container [ ] [ navBrand ]
-            ]
-        ]
-
-        Hero.body [ ] [
-            Container.container [ ] [
-                Column.column [
-                    Column.Width (Screen.All, Column.Is6)
-                    Column.Offset (Screen.All, Column.Is3)
-                ] [
-                    Heading.p [ Heading.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [ str "consequences" ]
-                    Box.box' [ ] [
-                        Field.div [ Field.IsGrouped ] [
-                            Control.p [ ] [
-                                Button.a [
-                                    Button.Color IsPrimary
-                                    Button.OnClick (fun _ -> dispatch StartCreatingRoom)
-                                ] [
-                                    str "Create a room"
-                                ]
-                            ]
+    Container.container [ ] [
+        Column.column [
+            Column.Width (Screen.All, Column.Is6)
+            Column.Offset (Screen.All, Column.Is3)
+        ] [
+            Heading.p [ Heading.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [ str "consequences" ]
+            Box.box' [ ] [
+                Field.div [ Field.IsGrouped ] [
+                    Control.p [ ] [
+                        Button.a [
+                            Button.Color IsPrimary
+                            Button.OnClick (fun _ -> dispatch StartCreatingRoom)
+                        ] [
+                            str "Create a room"
                         ]
                     ]
                 ]
