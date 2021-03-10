d4e223cb8bfbf93ae11e2454091e9e4dba4f6626
Matt <mjg1992@hotmail.co.uk>
2021-03-10 09:29:59 +0000

    #16 Add Farmer project for automated Azure deploys

---
+++ src/Deployment/Program.fs
@@ -0,0 +1,16 @@
+﻿open Farmer
+open Farmer.Builders
+
+let app = webApp {
+    name "safe-consequences"
+    zip_deploy @"..\..\deploy"
+}
+
+let deployment = arm {
+    location Location.NorthEurope
+    add_resource app
+}
+
+deployment
+|> Deploy.execute "safe-consequences-group" Deploy.NoParameters
+|> ignore
